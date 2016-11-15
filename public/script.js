191497e28340d72c92adbf6a168e880f798a24b1
Cox <jonathan.cox.c@gmail.com>
2016-11-15 18:07:27 -0500

    getting data correctly now

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-// Dom refs
+// DOM refs
 $grid = $('#overlay-grid');
 
 // Get the readings from a JSON file
@@ -7,15 +7,13 @@ function getReadings() {
     url: './aq_readings.json',
     dataType: 'json',
     success: function(res) {
-      addGrid(res);
+      addGrids(res);
     }
   })
 }
 
 function addGrids(readings) {
   // readingDivs will hold the grids for each hourly report
-  var readingDivs = [];
-
   // Loop through the reports for each hour since Nov 11
   for (var i=0; i<readings.length; i++) {
     // Each reading contains a time and a list of 3 lists of intensities for grid cells
@@ -25,39 +23,53 @@ function addGrids(readings) {
     var grid = readings[i]['grid']
     for (var j=0; j<3; j++) {
       var row = grid[j].map(function(cell) {
+        var pollution;
+
+        if (cell < 50) {
+          pollution = 'low';
+        }
+        else if (cell < 100) {
+          pollution = 'mid';
+        }
+        else if (cell < 150) {
+          pollution = 'high';
+        }
+
         return (`
-          <div class="grid-cell" data-intensity=${cell}></div>
+          <div class="grid-cell" data-pollution=${pollution}></div>
         `)
       });
-      readingDiv.push(`div class="row">${row.join('\n')}</div>`);
+      readingDiv.push(`<div class="row">${row.join('\n')}</div>`);
     };
 
     freadingDiv = readingDiv.join('\n');
-    readingDivs.push(`<div class="aq-reading" data-hour=${readings[i]['time']}>
+    $grid.append(`<div class="aq-reading" data-hour=${readings[i]['time']}>
       ${freadingDiv}
     </div>`);
   }
-  // Append all the grids to the holder. Will display the correct one with cycle()
-  $grid.html(readingDivs.join('\n'));
-  cycle(0);
+
+  $('.grid-cell').each(function() {
+    colors = {'low': 'green', 'mid': 'orange', 'high': 'red'};
+    p = this.dataset.pollution
+
+    $(this).css('background-color', colors[p]);
+  });
+
+  cycle(readings.length, 0);
 }
 
-function cycle(h) {
-  if (!readings) {
-    console.log('No AQI readings loaded.')
-  }
-  else {
-    $('.ag-reading').each(function() {
-        if (this.dataset.hour === hours[h]) {
-          $(this).show();
-      }
-      else $(this).hide();
-    })
+function cycle(num_hours, h) {
+  $('.aq-reading').each(function() {
+      if (this.dataset.hour === hours[h]) {
+        $(this).show();
+    }
+    else $(this).hide();
+  })
 
-    // Loop the animation every half-second
-    var next = (h === hours.length) ? -1 : h;
-    setTimeout(cycle(next+1), 500);
-  };
+  // Loop the animation every half-second
+  if (h != 10) {
+    setTimeout(cycle(num_hours, h+1), 500);
+  }
 }
 
 getReadings();

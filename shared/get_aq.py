148b09b40b9a88f4a73979cb7c8b084047ca3eab
Cox <jonathan.cox.c@gmail.com>
2016-11-15 15:50:16 -0500

    data added

--- shared/get_aq.py
+++ shared/get_aq.py
@@ -1,70 +1,13 @@
 #! /usr/bin/python
+import csv
 
-# Standard library imports
-import os
-import shutil
-import cgi
-import pdb
+station_coords = {
+        'South DeKalb': [33.6906, -84.2731],
+        'Confederate Ave': [33.7206, -84.3578],
+        'Gwinnett Tech': [33.9636, -84.0664],
+        'Yorkville': [33.9283, -85.0453],
+        'Newnan': [33.4039, -84.7461],
+        'McDonough': [33.4347, -84.1617]
+}
 
-# Third party imports
-import requests
-
-# Constants
-API_KEY = os.environ.get('AQ_KEY')
-DIST = 8 # The range of the air quality average
-
-# A three-by-three grid of latitude and longitude points, each the center of a
-# grid cell roughly 15 mi sq
-latlngs = [
-    [34.0708, -84.6071],
-    [34.0708, -84.3379],
-    [34.0708, -84.0797],
-
-    [33.8514, -84.6071],
-    [33.8514, -84.3379],
-    [33.8514, -84.0797],
-
-    [33.60203, -84.6071],
-    [33.60203, -84.3379],
-    [33.60203, -84.0797],
-]
-
-def get_aq(directory='/tmp/'):
-    url_template = 'http://www.airnowapi.org/aq/observation/latLong/historical/?format=application/xml&latitude={lat}&longitude={lng}&date={date}&distance={dist}&API_KEY={key}'
-    date = '2016-11-{}T00-0000' # The API only allows you to make a request for readings at midnight
-
-    for coords in latlngs:
-        # Loop through a list of dates to get air quality history
-        for day in [12, 13, 14, 15]:
-            context = {
-                'lat': coords[0],
-                'lng': coords[1],
-                'date': date.format(day),
-                'dist': 25,
-                'key': API_KEY
-            }
-            url = url_template.format(**context)
-
-            res = requests.get(url)
-
-            if res.status_code != 200:
-                raise ValueError('Failed to download')
-
-            params = cgi.parse_header(
-                res.headers.get('Content-Disposition', ''))[-1]
-
-            pdb.set_trace()
-
-            # The API returns a CSV so we need to write it to a file
-            fname = params['filename'] # Should be Output.csv but we don't want to take any chances
-            abs_path = os.path.join(directory, fname)
-            pdb.set_trace()
-            with open(abs_path, 'wb') as f:
-                res.raw.decode_content = True
-                shutil.copyfileobj(res.raw, f)
-
-            pdb.set_trace()
-
-if __name__ == "__main__":
-    get_aq()
 
